314dafccb2dd6aed472362fb4486d17535f4eccf
xiaxuanzhe <3058634081@qq.com>
2024-01-01 18:14:02 +0100

    add employee(Fonc)

--- src/main/java/com/xuanzhe/springbootproj/controller/EmployeeController.java
+++ src/main/java/com/xuanzhe/springbootproj/controller/EmployeeController.java
@@ -1,21 +1,40 @@
 package com.xuanzhe.springbootproj.controller;
 
+import com.xuanzhe.springbootproj.dao.DepartmentDao;
 import com.xuanzhe.springbootproj.dao.EmployeeDao;
+import com.xuanzhe.springbootproj.pojo.Department;
 import com.xuanzhe.springbootproj.pojo.Employee;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmployeeController {
   @Autowired
   EmployeeDao employeeDao;
+  @Autowired
+  DepartmentDao departmentDao;
   @RequestMapping("/emps")
   public String list(Model model){
     Collection<Employee> employees = employeeDao.getAll();
     model.addAttribute("emps",employees);
     return "emp/list";
+  }
+
+  @GetMapping("emp")
+  public String ToAddpage(Model model){
+    Collection<Department> departments = departmentDao.getDepartments();
+    model.addAttribute("departments",departments);
+    return "emp/add";
+  }
+
+  @PostMapping("/emp")
+  public String addEmp(Employee employee){
+    employeeDao.save(employee);
+    return "redirect:/emps";
   }
 }
